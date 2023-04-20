cced2f1c5c8bab73eee4fb0f47b66fcf30b1e309
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-04-19 19:10:57 -0500

    Formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Formulario } from './components/Formulario';
 
 export const App = () => {
   return (
@@ -11,6 +12,8 @@ export const App = () => {
         <div className="col-md-6">
           <h1 className="ms-5 mt-5 text-light fw-bold">Cotiza CriptoMonedas al Instante</h1>
           <hr className="w-25 border border-primary border-3 opacity-100 ms-5"></hr>
+
+          <Formulario />
         </div>
       </div>
 
