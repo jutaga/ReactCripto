3595f5cdce21ac8a18be6e5af825945c65da8ee3
Juan Pablo Tabares <juancho9615@hotmail.com>
2023-04-19 19:03:15 -0500

    Visuals

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+export const App = () => {
+  return (
+    <div className="container">
+
+      <div className="row">
+        <div className="col-md-6 ">
+          <img className="img-fluid mt-5" src="./src/cryptomonedas.png" alt="crypto" />
+        </div>
+
+        <div className="col-md-6">
+          <h1 className="ms-5 mt-5 text-light fw-bold">Cotiza CriptoMonedas al Instante</h1>
+          <hr className="w-25 border border-primary border-3 opacity-100 ms-5"></hr>
+        </div>
+      </div>
+
+    </div>
+
+
+  )
+}
